85ccd1c0b6ba9120fa9872bb09f5da354906d98a
Santiago Rodriguez <83855468+Ariodriguez@users.noreply.github.com>
2023-02-11 03:59:01 -0300

    main view finished and edit view started

--- src/components/Empleados.js
+++ src/components/Empleados.js
@@ -1,71 +1,17 @@
 import React, { useEffect } from 'react'
-import { CardContent, Grid, TextField } from '@mui/material';
-import InputAdornment from '@mui/material/InputAdornment';
-import SearchIcon from '@mui/icons-material/Search';
-import { DataGrid } from '@mui/x-data-grid';
-//Componentes
-import Empleado from './Empleado';
+import { TableContainer, Table, TableHead, TableCell, TableRow, TableBody } from '@mui/material';
+import { Link } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Chip from '@mui/material/Chip';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 //Redux
 import { useSelector, useDispatch } from 'react-redux';
 import { obtenerEmpleadosAction } from '../actions/empleadoActions';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'email',
-    headerName: 'email',
-    type: 'email',
-    width: 160,
-  },
-  {
-    field: 'phone_number',
-    headerName: 'Phone Number',
-    type: 'number',
-    width: 160
-  },
-  {
-    field: 'hireDate',
-    headerName:'Hire Date',
-    type: 'date',
-    with: 160
-  },
-  {
-    field: 'salary',
-    headerName: 'Salary',
-    type: 'number',
-    width: 160
-  },
-  {
-    field:'comission',
-    headerName: 'Comission',
-    type: 'number',
-    with: 160
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-  
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', email: 'jon@bastard.com' },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', email:'principe@encantador', hireDate: 10.02 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', hireDate: 5.09 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', hireDate: 6.08 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', hireDate: 7.10 },
-  { id: 6, lastName: 'Melisandre', firstName: null, hireDate: 15.03 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', hireDate: 4.04 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', hireDate: 3.06 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', hireDate: 6.05 },
-];
 
 function Empleados() {
 
@@ -77,12 +23,12 @@ function Empleados() {
     const cargarEmpleados = () => dispatch( obtenerEmpleadosAction() );
     cargarEmpleados()
 
-  }, [])
+  }, [])  
 
   //Obtener el State
   const empleados = useSelector( state => state.empleados.empleados )
-  console.log(empleados)
-
+  const error = useSelector(state => state.empleados.error)
+  
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -90,47 +36,94 @@ function Empleados() {
             Agregar Nuevo Empleado
       </h2>
       </div>
-        {empleados.length === 0 ? 'No hay empleados' : (
-          empleados.map(empleado => (
-            <Empleado
-              key={empleado.id}
-              empleado={empleado}
-            />
-          ))
-        )}
-        <Grid alignItems="center" container spacing={3}>
-            <Grid item xs={6} lg={4} md={4} marginLeft={6}>
-                <TextField
-                  InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SearchIcon />
-                    </InputAdornment>
-                  )
-                  }}
-                  sx={{
-                    m: 0
-                  }}
-/*                   onChange={handleQueryChange}
-                  value={query} */
-                  fullWidth
-                  placeholder='Buscar empleados...'
-                  variant="outlined"
-                />
-            </Grid>
-          </Grid>
+          <>
           <br></br>
         <div style={{ display: 'flex', justifyContent: 'center'  }}>
-        <CardContent sx={{ height: 400, width: '100%'}}>
-              <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-              />
-        </CardContent>
+        <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align='center'>ID</TableCell>
+                    <TableCell align='center'>NOMBRE</TableCell>
+                    <TableCell align='center'>APELLIDO</TableCell>
+                    <TableCell align='center'>EMAIL</TableCell>
+                    <TableCell align='center'>TELEFONO</TableCell>
+                    <TableCell align='center'>FECHA DE ALTA</TableCell>
+                    <TableCell align='center'>SALARIO</TableCell>
+                    <TableCell align='center'>COMISIÓN</TableCell>
+                    <TableCell align='center'>ACCIÓN</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {empleados.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      hover>
+                      <TableCell align='center' component='th' scope='row'>
+                        {row.id}
+                      </TableCell>
+                      <TableCell align='center' component='th' scope='row'>
+                        {row.nombre}
+                      </TableCell>
+                      <TableCell align='center' component='th' scope='row'>
+                        {row.apellido}
+                      </TableCell>
+                      <TableCell align='center'>
+                        {row.email}
+                      </TableCell>
+                      <TableCell align='center'>
+                        {row.telefono}
+                      </TableCell>
+                      <TableCell align='center'>
+                        {row.fechaAlta}
+                      </TableCell>
+                      <TableCell align='center'>
+                        ${row.salario}
+                      </TableCell>
+                      <TableCell align='center'>
+                          <div style={{ display: 'flex', justifyContent: 'center'}} >
+                            <Chip 
+                              size="small" 
+                              color="primary" 
+                              value={row.comision} 
+                              label={`% ${row.comision}`}
+                            /> 
+                          </div>
+                      </TableCell>
+
+                      {/* botones de accion, editar y eliminar */}
+
+                      <TableCell align='center'>
+                      <Button>
+                        <Link to={`/employees/edit/${row.id}`} >
+                        <EditIcon
+                          style={{ color: '#ffba00' }}
+                        />
+                        </Link>
+                      </Button>
+                      <Button>
+                      <DeleteIcon
+                         style={{ color: '#ff6961' }}
+                        />
+                      </Button>
+                      </TableCell>
+
+                    </TableRow>
+                  ))}
+
+                </TableBody>
+              </Table>
+            </TableContainer>
         </div>
+
+        <br></br>            
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Stack spacing={2}>
+          <Pagination count={5} defaultPage={1} color="primary" />
+        </Stack>
+        </div>
+        <br></br>
+    </>
     </>
   )
 }
